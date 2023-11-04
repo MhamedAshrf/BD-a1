c17b6ce76d92845d8d496d8c3c15981f676c073c
Mohammed Ashraf <M.abdulsami@nu.edu.eg>
2023-11-04 22:05:16 +0200

    removed unnecessary lines

--- final.sh
+++ final.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-
-# container=$(docker ps -a | grep "bd-a1" | awk '{ print $1 }')
-
-# user=$(whoami)
-# workingdirectory=$(pwd)
-
-# # -d is for checking if a direcory exists
-# if [ ! -d $(pwd)/service-result ]; then
-#     mkdir  $(pwd)/service-result
-# fi
-
-# #to copy the files from cont to host
-# docker cp   $container:/home/doc-bd-a1/res_dpre.csv service-result/
-# docker cp   $container:/home/doc-bd-a1/da-in-*.txt service-result/
-
-
-# docker container stop $container
-
-
 PPID=$(ps -p $$ | awk '{print $1}')
 
 
